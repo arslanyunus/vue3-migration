7ce23b8568cca383ada7500e33bdbdd7715d323a
Yunus Arslan <yunus@mobileaction.co>
2020-12-17 18:34:42 +0300

    no need en locale, it's default now

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,5 @@
 import ElementPlus from 'element-plus';
 import '../element-variables.scss';
 import 'element-plus/lib/theme-chalk/index.css';
-import 'element-plus/lib/locale/lang/en';
 
 export default ElementPlus;
